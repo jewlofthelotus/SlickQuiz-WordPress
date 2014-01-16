299cedda385e40c8b4e8730ca6ad1b10bdb21c08
Patrick Holberg Hesselberg <phh@peytz.dk>
2014-01-16 16:29:50 +0100

    adds filter: slickquiz_after_result

--- php/slickquiz-front.php
+++ php/slickquiz-front.php
@@ -261,9 +261,9 @@ if ( !class_exists( 'SlickQuizFront' ) ) {
                                     <div class="quizShare"></div>';
                     }
 
-                    $out .= '
-                                </div>
-                            </div>
+                    $out .= '</div>';
+					$out .= apply_filters( 'slickquiz_after_result', $this );
+					$out .= '</div>
                         </div>';
                 }
             } else {
